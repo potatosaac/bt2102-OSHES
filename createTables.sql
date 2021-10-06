1794d9461a2a4187fa83cf9b1a96c14d4a88173a
potatosaac <66517626+potatosaac@users.noreply.github.com>
2021-10-06 14:06:02 +0800

    Add files via upload

---
+++ createTables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE CUSTOMER(
+	CustomerID   VARCHAR(120) NOT NULL,
+    fName        VARCHAR(120) NOT NULL,
+    Gender       VARCHAR(120) CONSTRAINT GenderType CHECK(Gender IN ('M', 'F')),
+    eMail        VARCHAR(120),
+    phoneNo      VARCHAR(120),
+    address      VARCHAR(120),
+    custPassword VARCHAR(120) NOT NULL,
+    PRIMARY KEY (CustomerID));
+
+CREATE TABLE ADMINISTRATOR(
+	AdminID       VARCHAR(120) NOT NULL,
+    fName         VARCHAR(120) NOT NULL,
+    Gender        VARCHAR(120) CONSTRAINT GenderType CHECK(Gender IN ('M', 'F')),
+    phoneNo       VARCHAR(120),
+    adminPassword VARCHAR(120) NOT NULL,
+    PRIMARY KEY (AdminID));
+
+CREATE TABLE MODEL(
+	modelName     VARCHAR(120) NOT NULL CONSTRAINT modelNameType CHECK(modelName IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
+    price         DECIMAL(6,2) NOT NULL,
+    cost          DECIMAL(6,2) NOT NULL,
+    warranty      SMALLINT,
+    PRIMARY KEY (modelName));
+    
+CREATE TABLE PRODUCT(
+	ProductID         VARCHAR(120) NOT NULL,
+    modelName         VARCHAR(120) CONSTRAINT modelNameType CHECK(modelName IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
+    purchaseStatus    VARCHAR(120) NOT NULL CONSTRAINT purchaseStatusType CHECK(purchaseStatus IN ('Sold', 'Unsold')),
+    specificInfo      VARCHAR(120) NOT NULL,
+    PRIMARY KEY (ProductID),
+    FOREIGN KEY (ModelName) REFERENCES MODEL(modelName) ON DELETE SET NULL
+														ON UPDATE CASCADE);
+
+	
