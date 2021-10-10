8f00b39b60a9404cefcf86b0d4d0b0ee6b9f702f
bobby-ui <64159260+bobby-ui@users.noreply.github.com>
2021-10-10 22:55:04 +0800

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE CUSTOMER(
 	CustomerID   VARCHAR(120) NOT NULL,
     fName        VARCHAR(120) NOT NULL,
-    Gender       VARCHAR(120) CONSTRAINT GenderType CHECK(Gender IN ('M', 'F')),
+    Gender       VARCHAR(120) CONSTRAINT CHECK(Gender IN ('M', 'F')),
     eMail        VARCHAR(120),
     phoneNo      VARCHAR(120),
     custAddress  VARCHAR(120),
@@ -11,50 +11,28 @@ CREATE TABLE CUSTOMER(
 CREATE TABLE ADMINISTRATOR(
 	AdminID       VARCHAR(120) NOT NULL,
     fName         VARCHAR(120) NOT NULL,
-    Gender        VARCHAR(120) CONSTRAINT GenderType CHECK(Gender IN ('M', 'F')),
+    Gender        VARCHAR(120) CONSTRAINT CHECK(Gender IN ('M', 'F')),
     phoneNo       VARCHAR(120),
     adminPassword VARCHAR(120) NOT NULL,
-    PRIMARY KEY (AdminID));
-
-CREATE TABLE MODEL(
-	modelName     VARCHAR(120) NOT NULL CONSTRAINT modelNameType CHECK(modelName IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
-    category      VARCHAR(120) NOT NULL CONSTRAINT categoryType CHECK(category IN ('Lights', 'Locks')),
-    price         DECIMAL(6,2) NOT NULL,
-    cost          DECIMAL(6,2) NOT NULL,
-    warranty      SMALLINT,
-    PRIMARY KEY (modelName, category));
-    
-CREATE TABLE PRODUCT(
-	ProductID         VARCHAR(120) NOT NULL,
-    modelName         VARCHAR(120) CONSTRAINT modelNameType CHECK(modelName IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
-    purchaseStatus    VARCHAR(120) NOT NULL CONSTRAINT purchaseStatusType CHECK(purchaseStatus IN ('Sold', 'Unsold')),
-    specificInfo      VARCHAR(120) NOT NULL,
-    PRIMARY KEY (ProductID),
-    FOREIGN KEY (modelName, category) REFERENCES MODEL(modelName, category) ON DELETE SET NULL
-																			
+    PRIMARY KEY (AdminID));																		
 																			 
 CREATE TABLE ITEMS(
 	ItemID		VARCHAR(120) NOT NULL,
-	Model		VARCHAR(120) NOT NULL CONSTRAINT ModelType CHECK(Model IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
-    	Category	VARCHAR(120) NOT NULL CONSTRAINT CategoryType CHECK(Category IN ('Lights', 'Locks')),
-    	Color		VARCHAR(120) NOT NULL,
-    	Factory		VARCHAR(120) NOT NULL,
+	Model		VARCHAR(120) NOT NULL CONSTRAINT CHECK(Model IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
+	Category	VARCHAR(120) NOT NULL CONSTRAINT CHECK(Category IN ('Lights', 'Locks')),
+	Color		VARCHAR(120) NOT NULL,
+	Factory		VARCHAR(120) NOT NULL,
 	PowerSupply	VARCHAR(120) NOT NULL,  
 	PurchaseStatus	VARCHAR(120) NOT NULL CONSTRAINT purchaseStatusType CHECK(purchaseStatus IN ('Sold', 'Unsold')),
 	ProductionYear	INT NOT NULL,
-	Model		VARCHAR(120) NOT NULL,	
 	ServiceStatus	VARCHAR(120),
-    	PRIMARY KEY (ItemID),
-	FOREIGN KEY (Model));
+	PRIMARY KEY (ItemID));
     
-CREATE TABLE PRODUCT(
-	ProductID	VARCHAR(120) NOT NULL,
-	Cost		INT NOT NULL,
-    	Model		VARCHAR(120) CONSTRAINT ModelType CHECK(Model IN ('Light1', 'Light2', 'Safe1', 'Safe2', 'Safe3', 'SmartHome1')),
-    	Price		INT NOT NULL, 
-    	Category	VARCHAR(120) NOT NULL,
-	Warrenty(months)SMALLINT NOT NULL,
-    	PRIMARY KEY (ProductID),
-    	FOREIGN KEY (Model) REFERENCES ITEMS(Model) ON DELETE SET NULL
-
-																	ON UPDATE CASCADE);
+CREATE TABLE PRODUCTS (
+  `Category` text NOT NULL,
+  `Cost ($)` int NOT NULL,
+  `Model` text NOT NULL,
+  `Price ($)` int NOT NULL,
+  `ProductID` int NOT NULL,
+  `Warranty (months)` int NOT NULL,
+  PRIMARY KEY (`ProductID`));
